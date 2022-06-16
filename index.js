d7bee0464fcdc1f4a1f44bec7d6954a046b8c073
stensis <irenenjuguna98@gmail.com>
2022-06-16 12:51:09 +0300

    commit tree 7daf8872c912443c312c1c98b5c00866195f3911

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+     const input = document.getElementById('input');
+    input.addEventListener('click', function(){alert('I was clicked')});
+}
+//callback: arrow function
+function addingEventListenerArr(){
+    const input = document.getElementById('input');
+    input.addEventListener('click',function() {
+        alert('I was clicked!');
+    }); 
+    input.addEventListener('click', () => alert('I was clicked'));
 }
